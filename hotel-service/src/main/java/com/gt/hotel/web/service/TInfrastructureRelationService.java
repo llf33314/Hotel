7021dd346f399918bb0ca5d331bee70602552066
Reverien <Reverien9@gmail.com>
2017-10-11 18:17:05 +0800

    2017-10-11 18:16:58

--- hotel-service/src/main/java/com/gt/hotel/web/service/TInfrastructureRelationService.java
+++ hotel-service/src/main/java/com/gt/hotel/web/service/TInfrastructureRelationService.java
@@ -8,9 +8,9 @@ import com.gt.hotel.entity.TInfrastructureRelation;
  * 基础设施表关系(酒店/房型 关联的基础属性都在这里) 服务类
  * </p>
  *
- * @author
- * @since 2017-10-09
+ * @author 
+ * @since 2017-10-11
  */
-public interface TInfrastructureRelationService extends BaseService< TInfrastructureRelation > {
-
+public interface TInfrastructureRelationService extends BaseService<TInfrastructureRelation> {
+	
 }
