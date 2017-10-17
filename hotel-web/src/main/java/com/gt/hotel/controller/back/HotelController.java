ed580ab72421301fdd022ea42126a52b3de31dfd
zhangmz <souldeathr@gmail.com>
2017-10-17 17:24:51 +0800

    修改Jrebel配置 测试代码 注释

--- hotel-web/src/main/java/com/gt/hotel/controller/back/HotelController.java
+++ hotel-web/src/main/java/com/gt/hotel/controller/back/HotelController.java
@@ -38,69 +38,76 @@ import java.util.List;
 @RequestMapping("/back/hotel")
 public class HotelController extends BaseController {
 
-    private static final Logger logger = LoggerFactory.getLogger(HotelController.class);
+		private static final Logger logger = LoggerFactory.getLogger(HotelController.class);
 
-    @Autowired
-    private THotelService tHotelService;
+		@Autowired
+		private THotelService tHotelService;
 
-    @Autowired
-    private WXMPApiUtil WXMPApiUtil;
+		@Autowired
+		private WXMPApiUtil WXMPApiUtil;
 
-    @Value("${wxmp.imageurl.prefixurl}")
-    private String IMAGE_PREFIX;
+		@Value("${wxmp.imageurl.prefixurl}")
+		private String IMAGE_PREFIX;
 
-    @ApiOperation(value = "门店列表", notes = "门店列表") @ApiResponses({ @ApiResponse(code = 0, message = "门店列表", response = HotelShopInfo.class) })
-    @GetMapping(value = "queryShop", produces = MediaType.APPLICATION_JSON_UTF8_VALUE) @SuppressWarnings("rawtypes") public ResponseDTO shopR(HttpSession session) {
-	Integer busid = getLoginUserId(session);
-	List< HotelWsWxShopInfoExtend > shops = null;
-	try {
-	    shops = WXMPApiUtil.queryWxShopByBusId(busid);
-	    List< HotelShopInfo > s = new ArrayList<>();
-	    for (HotelWsWxShopInfoExtend shop : shops) {
-		HotelShopInfo _s = new HotelShopInfo();
-		_s.setShopid(shop.getId());
-		_s.setName(shop.getBusinessName());
-		_s.setTel(shop.getTelephone());
-		_s.setAddr(shop.getAddress());
-		_s.setImage(IMAGE_PREFIX + shop.getImageUrl());
-		s.add(_s);
-	    }
-	    return ResponseDTO.createBySuccess(s);
-	} catch (SignException e) {
-	    logger.error("签名错误：{}", e.getMessage());
-	    throw new ResponseEntityException(ResponseEnums.SIGNATURE_ERROR);
-	}
-    }
+		@ApiOperation(value = "门店列表", notes = "门店列表")
+		@ApiResponses({ @ApiResponse(code = 0, message = "门店列表", response = HotelShopInfo.class) })
+		@GetMapping(value = "queryShop", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+		@SuppressWarnings("rawtypes")
+		public ResponseDTO shopR(HttpSession session) {
+				Integer busid = getLoginUserId(session);
+				List< HotelWsWxShopInfoExtend > shops = null;
+				try {
+						shops = WXMPApiUtil.queryWxShopByBusId(busid);
+						List< HotelShopInfo > s = new ArrayList<>();
+						for (HotelWsWxShopInfoExtend shop : shops) {
+								HotelShopInfo _s = new HotelShopInfo();
+								_s.setShopid(shop.getId());
+								_s.setName(shop.getBusinessName());
+								_s.setTel(shop.getTelephone());
+								_s.setAddr(shop.getAddress());
+								_s.setImage(IMAGE_PREFIX + shop.getImageUrl());
+								s.add(_s);
+						}
+						return ResponseDTO.createBySuccess(s);
+				} catch (SignException e) {
+						logger.error("签名错误：{}", e.getMessage());
+						throw new ResponseEntityException(ResponseEnums.SIGNATURE_ERROR);
+				}
+		}
 
-    @ApiOperation(value = "酒店列表", notes = "酒店列表")
-    @ApiResponses({ @ApiResponse(code = 0, message = "分页对象", response = ResponseDTO.class), @ApiResponse(code = 1, message = "酒店列表", response = HotelVo.class) })
-    @GetMapping(value = "queryHotel", produces = MediaType.APPLICATION_JSON_UTF8_VALUE) @SuppressWarnings("rawtypes") public ResponseDTO hotelR(Query hpage, HttpSession session) {
-	Page< HotelVo > page = new Page<>(hpage.getPage(), hpage.getPageSize());
-	Integer busid = getLoginUserId(session);
-	page = tHotelService.queryHotelHome(busid, hpage, page);
-	return ResponseDTO.createBySuccess(page);
-    }
+		@ApiOperation(value = "酒店列表2222212", notes = "酒店列表1")
+		@ApiResponses({ @ApiResponse(code = 0, message = "分页对象", response = ResponseDTO.class), @ApiResponse(code = 1, message = "酒店列表", response = HotelVo.class) })
+		@GetMapping(value = "queryHotel", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+		@SuppressWarnings("rawtypes")
+		public ResponseDTO hotelR(Query hpage, HttpSession session) {
+				Page< HotelVo > page = new Page<>(hpage.getPage(), hpage.getPageSize());
+				Integer busid = getLoginUserId(session);
+				page = tHotelService.queryHotelHome(busid, hpage, page);
+				return ResponseDTO.createBySuccess(page);
+		}
 
-    @ApiOperation(value = "新增或更新酒店", notes = "新增或更新酒店") @ApiResponses({ @ApiResponse(code = 0, message = "", response = ResponseDTO.class) })
-    @PostMapping(value = "insertHotel", produces = MediaType.APPLICATION_JSON_UTF8_VALUE) @SuppressWarnings("rawtypes")
-    public ResponseDTO hotelCU(@Validated @ModelAttribute HotelParameter.SaveOrUpdate hotel, BindingResult bindingResult, HttpSession session) {
-	InvalidParameter(bindingResult);
-	Integer busid = getLoginUserId(session);
-	Date date = new Date();
-	THotel e = new THotel();
-	BeanUtils.copyProperties(hotel, e);
-	e.setId(hotel.getHotelId());
-	e.setPhone(hotel.getTel());
-	e.setAddress(hotel.getAddr());
-	e.setStoreId(hotel.getShopId());
-	e.setCreatedBy(busid);
-	e.setCreatedAt(date);
-	e.setUpdatedBy(busid);
-	e.setUpdatedAt(date);
-	if (e.insertOrUpdate())
-	    return ResponseDTO.createBySuccess();
-	else
-	    return ResponseDTO.createByError();
-    }
+		@ApiOperation(value = "新增或更新酒店", notes = "新增或更新酒店")
+		@ApiResponses({ @ApiResponse(code = 0, message = "", response = ResponseDTO.class) })
+		@PostMapping(value = "insertHotel", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+		@SuppressWarnings("rawtypes")
+		public ResponseDTO hotelCU(@Validated @ModelAttribute HotelParameter.SaveOrUpdate hotel, BindingResult bindingResult, HttpSession session) {
+				InvalidParameter(bindingResult);
+				Integer busid = getLoginUserId(session);
+				Date date = new Date();
+				THotel e = new THotel();
+				BeanUtils.copyProperties(hotel, e);
+				e.setId(hotel.getHotelId());
+				e.setPhone(hotel.getTel());
+				e.setAddress(hotel.getAddr());
+				e.setStoreId(hotel.getShopId());
+				e.setCreatedBy(busid);
+				e.setCreatedAt(date);
+				e.setUpdatedBy(busid);
+				e.setUpdatedAt(date);
+				if (e.insertOrUpdate())
+						return ResponseDTO.createBySuccess();
+				else
+						return ResponseDTO.createByError();
+		}
 
 }
