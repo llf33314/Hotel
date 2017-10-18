49fbbf08ce364622015c8c52ce5ab92fe19afde8
zhangmz <souldeathr@gmail.com>
2017-10-18 10:08:06 +0800

    删除不必要的，更新gt_hotel database 数据库的更新

--- hotel-web/src/main/java/com/gt/hotel/config/WebMvcConfig.java
+++ hotel-web/src/main/java/com/gt/hotel/config/WebMvcConfig.java
@@ -20,9 +20,10 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
     private static final String LOGIN_URL = "/backstage/home";
 
     // 快速解决页面转向问题
-    @Override public void addViewControllers(ViewControllerRegistry registry) {
-	registry.addViewController("/").setViewName("/nav.html");
-	registry.addViewController("/error").setViewName("/error/defaultError.html");
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("/nav.html");
+        registry.addViewController("/error").setViewName("/error/defaultError.html");
     }
 
     /**
@@ -30,40 +31,46 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
      *
      * @param registry ResourceHandlerRegistry
      */
-    @Override public void addResourceHandlers(ResourceHandlerRegistry registry) {
-	registry.addResourceHandler("/**").addResourceLocations("classpath:/pages/").addResourceLocations("classpath:/templates/").addResourceLocations("classpath:/static/");
-	registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
-	registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
-	//		super.addResourceHandlers(registry);
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").addResourceLocations("classpath:/pages/").addResourceLocations("classpath:/templates/").addResourceLocations("classpath:/static/");
+        registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+        //		super.addResourceHandlers(registry);
     }
 
-    @Override public void addInterceptors(InterceptorRegistry registry) {
-	InterceptorRegistration addInterceptor = registry.addInterceptor(getSecurityInterceptor());
-	addInterceptor.addPathPatterns("/**");
-	addInterceptor.excludePathPatterns("/backstage/home", "/error", "/v2/**", "/webjars/**", "/swagger-resources/**");
-	//        super.addInterceptors(registry);
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        InterceptorRegistration addInterceptor = registry.addInterceptor(getSecurityInterceptor());
+        addInterceptor.addPathPatterns("/**");
+        addInterceptor.excludePathPatterns("/backstage/home", "/error", "/v2/**", "/webjars/**", "/swagger-resources/**");
+        //        super.addInterceptors(registry);
     }
 
-    @Bean public SecurityInterceptor getSecurityInterceptor() {
-	return new SecurityInterceptor();
+    @Bean
+    public SecurityInterceptor getSecurityInterceptor() {
+        return new SecurityInterceptor();
     }
 
     private class SecurityInterceptor extends HandlerInterceptorAdapter {
-	@Override public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-	    System.err.println(request.getLocalAddr() + " " + request.getRequestURL());
-	    /*if (SessionUtils.getLoginUser(request) != null)*/
-	    return true;
-	    //            response.sendRedirect(LOGIN_URL);
-	    //            return false;
-	}
+        @Override
+        public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+            System.err.println(request.getLocalAddr() + " " + request.getRequestURL());
+        /*if (SessionUtils.getLoginUser(request) != null)*/
+            return true;
+            //            response.sendRedirect(LOGIN_URL);
+            //            return false;
+        }
     }
 
-    @Bean public WebMvcConfigurer corsConfigurer() {
-	return new WebMvcConfigurerAdapter() {
-	    @Override public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**").allowedMethods("*").allowedOrigins("*");
-	    }
-	};
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedMethods("*").allowedOrigins("*");
+            }
+        };
     }
 
 }
