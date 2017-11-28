555bd4d6d1169087302acf73b0ce5b92113ba9b3
Reverien <Reverien9@gmail.com>
2017-11-28 11:25:39 +0800

    2017年11月28日11:25:35

--- hotel-web/src/main/java/com/gt/hotel/interceptor/MobileAuthenticationInterceptor.java
+++ hotel-web/src/main/java/com/gt/hotel/interceptor/MobileAuthenticationInterceptor.java
@@ -97,13 +97,13 @@ public class MobileAuthenticationInterceptor extends HandlerInterceptorAdapter {
     	SessionUtils.getLoginMember(request, 33);
     	
     	//test
-    	Member member1 = new Member();
-    	member1.setId(1071);
-    	member1.setBusid(33);
-    	member1.setPhone("13433550667");
-    	member1.setPublicId(492);
-    	member1.setCardid("15338");
-    	SessionUtils.setLoginMember(request, member1);
+//    	Member member1 = new Member();
+//    	member1.setId(1071);
+//    	member1.setBusid(33);
+//    	member1.setPhone("13433550667");
+//    	member1.setPublicId(492);
+//    	member1.setCardid("15338");
+//    	SessionUtils.setLoginMember(request, member1);
     	//test
     	
     	String hotelInfoJson = (String) request.getSession().getAttribute(CURRENT_HOTEL_INFO);
