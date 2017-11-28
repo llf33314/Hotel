bc62ef12b3c34032b0e2bfa46ce992b36e43bd21
Reverien <Reverien9@gmail.com>
2017-11-28 11:26:23 +0800

    2017年11月28日11:25:35

--- hotel-web/src/test/java/com/gt/hotel/service/HotelServiceImplTest.java
+++ hotel-web/src/test/java/com/gt/hotel/service/HotelServiceImplTest.java
@@ -1,24 +1,31 @@
 package com.gt.hotel.service;
 
-import com.baomidou.mybatisplus.plugins.pagination.PageHelper;
-import com.gt.hotel.BasicTest;
-import com.gt.hotel.entity.THotel;
-import com.gt.hotel.web.service.THotelService;
-import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.util.Assert;
-
 import java.util.List;
 
-/**
- * @author zhangmz
- * @version 1.0.0
- * @date 2017/10/10
- */
-public class HotelServiceImplTest extends BasicTest {
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.util.Assert;
+
+import com.baomidou.mybatisplus.plugins.pagination.PageHelper;
+import com.gt.hotel.HotelApplication;
+import com.gt.hotel.entity.THotel;
+import com.gt.hotel.util.WXMPApiUtil;
+import com.gt.hotel.web.service.THotelService;
+
+@RunWith(SpringJUnit4ClassRunner.class) 
+@SpringBootTest(classes = HotelApplication.class)
+@WebAppConfiguration
+public class HotelServiceImplTest {
 
     @Autowired
     private THotelService tHotelService;
+    
+    @Autowired
+    private WXMPApiUtil WXMPApiUtil;
 
     @Test
     public void queryHotelHome() throws Exception {
@@ -30,6 +37,15 @@ public class HotelServiceImplTest extends BasicTest {
             System.out.println(hotels.get(i).getAddress());
         }
 
+    }
+    
+    @Test
+    public void wxmp() throws Exception {
+    	try {
+			System.err.println(WXMPApiUtil.getMenus(0, 33, 0, null));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
     }
 
 }
