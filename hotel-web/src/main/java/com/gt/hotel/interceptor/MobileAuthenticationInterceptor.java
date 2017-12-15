675d447683ca9e267ae45a2109bed41c0e747dac
zhangmz <souldeathr@gmail.com>
2017-12-15 11:31:30 +0800

    修改部分

--- hotel-web/src/main/java/com/gt/hotel/interceptor/MobileAuthenticationInterceptor.java
+++ hotel-web/src/main/java/com/gt/hotel/interceptor/MobileAuthenticationInterceptor.java
@@ -1,22 +1,5 @@
 package com.gt.hotel.interceptor;
 
-import static com.gt.hotel.constant.CommonConst.CURRENT_SESSION_BUS_ID;
-import static com.gt.hotel.constant.CommonConst.CURRENT_SESSION_HOTEL_ID;
-import static com.gt.hotel.constant.CommonConst.CURRENT_SESSION_HOTEL_INFO;
-
-import java.io.UnsupportedEncodingException;
-import java.net.URLDecoder;
-import java.net.URLEncoder;
-import java.util.Map;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.apache.commons.collections.MapUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.servlet.HandlerMapping;
-import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
-
 import com.alibaba.fastjson.JSONObject;
 import com.baomidou.mybatisplus.mapper.EntityWrapper;
 import com.baomidou.mybatisplus.mapper.Wrapper;
@@ -30,8 +13,17 @@ import com.gt.hotel.exception.ResponseEntityException;
 import com.gt.hotel.properties.WebServerConfigurationProperties;
 import com.gt.hotel.util.WXMPApiUtil;
 import com.gt.hotel.web.service.THotelService;
-
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections.MapUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerMapping;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
+
+import static com.gt.hotel.constant.CommonConst.*;
 
 /**
  * 移动端登录、微信授权拦截器
@@ -60,7 +52,7 @@ public class MobileAuthenticationInterceptor extends HandlerInterceptorAdapter {
     /**
      * 路径上获取酒店ID
      */
-    public static final String HOTEL_ID = "hotelId";
+    private static final String HOTEL_ID = "hotelId";
 
     /**
      * 进入方法前，判断接口是否需要登录
@@ -78,12 +70,8 @@ public class MobileAuthenticationInterceptor extends HandlerInterceptorAdapter {
         Optional<Integer> busId = Optional.fromNullable((Integer) request.getSession().getAttribute(CURRENT_SESSION_BUS_ID));
         Optional<Integer> sessionHotelId = Optional.fromNullable((Integer) request.getSession().getAttribute(CURRENT_SESSION_HOTEL_ID));
         Map attribute = (Map) request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
-        String uri = request.getRequestURI();
-        if(uri.indexOf("/mobile/78CDF1/home/") != -1) {
-        	return true;
-        }
-        if(uri.indexOf("/mobile/78CDF1/common/") != -1) {
-        	return true;
+        if (passUri(request)) {
+            return true;
         }
         // hotelId
         Optional<Integer> hotelId = Optional.of(MapUtils.getInteger(attribute, HOTEL_ID));
@@ -92,16 +80,14 @@ public class MobileAuthenticationInterceptor extends HandlerInterceptorAdapter {
             if (sessionHotelId.isPresent() && !hotelId.get().equals(sessionHotelId.get())) {
                 // 数据库查询
                 Wrapper<THotel> wrapper = new EntityWrapper<>();
-                wrapper.eq("id", hotelId.get()).eq("bus_id", busId.get()).eq("mark_modified", 0);
+                wrapper.eq("id", hotelId.get()).eq("bus_id", busId.get());
                 Optional<THotel> hotel = Optional.fromNullable(this.hotelService.selectOne(wrapper));
                 if (!hotel.isPresent()) {
                     log.warn("获取酒店信息失败 参数列表：hotelId : {} , busId : {} ", hotelId.get(), busId.get());
                     throw new ResponseEntityException(ResponseEnums.DATA_DOES_NOT_EXIST);
                 }
                 // 并重置更新缓存信息
-                request.getSession().setAttribute(CURRENT_SESSION_HOTEL_INFO, JSONObject.toJSONString(hotel));
-                request.getSession().setAttribute(CURRENT_SESSION_BUS_ID, hotel.get().getBusId());
-                request.getSession().setAttribute(CURRENT_SESSION_HOTEL_ID, hotelId.get());
+                resetSession(request, hotelId.get(), hotel.get());
             }
             // 获取会员信息
             Optional<Member> member = Optional.fromNullable(SessionUtils.getLoginMember(request, busId.get()));
@@ -118,9 +104,7 @@ public class MobileAuthenticationInterceptor extends HandlerInterceptorAdapter {
             if (!sessionHotelId.isPresent()) {
                 hotel = Optional.of(findHotelById(hotelId.get()));
                 // 并重置更新缓存信息
-                request.getSession().setAttribute(CURRENT_SESSION_HOTEL_INFO, JSONObject.toJSONString(hotel));
-                request.getSession().setAttribute(CURRENT_SESSION_BUS_ID, hotel.get().getBusId());
-                request.getSession().setAttribute(CURRENT_SESSION_HOTEL_ID, hotelId.get());
+                resetSession(request, hotelId.get(), hotel.get());
                 // 获取会员信息
                 member = Optional.fromNullable(SessionUtils.getLoginMember(request, hotel.get().getBusId()));
             } else {
@@ -128,17 +112,38 @@ public class MobileAuthenticationInterceptor extends HandlerInterceptorAdapter {
                 hotel = Optional.of(JSONObject.parseObject(hotelInfoJson.get(), THotel.class));
                 member = Optional.fromNullable(SessionUtils.getLoginMember(request, hotel.get().getBusId()));
             }
-            if (hotel.isPresent()) {
-                if (!member.isPresent()) {
-                    log.warn("member is null");
-                    String url = wxmpApiUtil.authorizeMember(hotel.get().getBusId());
-                    throw new NeedLoginException(ResponseEnums.NEED_LOGIN, hotel.get().getBusId(), hotel.get().getId(), url);
-                }
+            if (!member.isPresent()) {
+                log.warn("member is null");
+                String url = wxmpApiUtil.authorizeMember(hotel.get().getBusId());
+                throw new NeedLoginException(ResponseEnums.NEED_LOGIN, hotel.get().getBusId(), hotel.get().getId(), url);
             } else {
                 throw new ResponseEntityException(ResponseEnums.BAD_REQUEST);
             }
         }
         return true;
+    }
+
+    /**
+     * 匹配上请求地址 全都通过
+     * @param request HttpServletRequest
+     * @return boolean
+     */
+    private boolean passUri(HttpServletRequest request) {
+        String uri = request.getRequestURI();
+        return uri.contains("/mobile/78CDF1/home/") || uri.contains("/mobile/78CDF1/common/");
+    }
+
+    /**
+     * 重置Session
+     *
+     * @param request HttpServletRequest
+     * @param hotelId 酒店ID
+     * @param hotel   酒店信息
+     */
+    private void resetSession(HttpServletRequest request, Integer hotelId, THotel hotel) {
+        request.getSession().setAttribute(CURRENT_SESSION_HOTEL_INFO, JSONObject.toJSONString(hotel));
+        request.getSession().setAttribute(CURRENT_SESSION_BUS_ID, hotel.getBusId());
+        request.getSession().setAttribute(CURRENT_SESSION_HOTEL_ID, hotelId);
     }
 
     /**
@@ -163,15 +168,6 @@ public class MobileAuthenticationInterceptor extends HandlerInterceptorAdapter {
             log.error("异常信息 : {} ", ex);
             throw new ResponseEntityException(ResponseEnums.UNKNOWN_ERROR);
         }
-    }
-
-    public static void main(String[] args) throws UnsupportedEncodingException {
-        String url = "https://deeptel.com.cn//phoneLoginController/33/79B4DE7C/phonelogin.do?returnKey=hotel1510621956750#1231saldaslkdjaldj#31142222";
-        String urlencode = URLEncoder.encode(url, "utf-8");
-        String urldecode = URLDecoder.decode(urlencode, "utf-8");
-        System.out.println("原始地址：" + url);
-        System.out.println("加密后的地址：" + urlencode);
-        System.out.println("解密后的地址：" + urldecode);
     }
 
 
